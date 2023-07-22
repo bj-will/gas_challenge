c9cb30e5391a327a7bc13782ff79fe62c2a56ead
root <root@home.example.com>
2023-07-22 21:51:37 +0000

    optimize contract

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract gasChallenge {
-    //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    // Changed to Fixed-Size Array
+    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10];
     
     //Function to check for sum of array
     //No changes required in this function
@@ -21,9 +21,15 @@ contract gasChallenge {
         }
     }
     
-    //Implement Remaining Gas Optimization Techniques Here
-    //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+        // Caching state variable
+        uint[10] storage cachedNumbers = numbers;
+
+        // Implement uncheck block and change increment syntax
+        unchecked {
+            for (uint i = 0; i < cachedNumbers.length; ++i) {
+                cachedNumbers[i] = 0;
+            }
+        }
     }
- }
+}
